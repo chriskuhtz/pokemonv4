53f25a0344e6067ce87e6f4aecf712848ba16cf9
Chris Kuhtz <christopherkuhtz@gmail.com>
2023-10-15 16:09:37 +0200

    implement modal

---
+++ src/ui_components/Modal/Modal.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react';
+
+export const Modal = ({
+	open,
+
+	modalContent,
+	modalTitle,
+	onOpen,
+	buttonContent,
+	onCancel,
+}: {
+	open: boolean;
+	onOpen: () => void;
+	onCancel: () => void;
+	modalContent: ReactNode;
+	modalTitle: ReactNode;
+	buttonContent: ReactNode;
+}): JSX.Element => {
+	if (!open) {
+		return <button onClick={onOpen}>{buttonContent}</button>;
+	}
+	return (
+		<div className="modal">
+			<div className="modalContent">
+				<div className="modalHeader">
+					<div>{modalTitle}</div>
+					<button onClick={onCancel}>X</button>
+				</div>
+
+				{modalContent}
+			</div>
+		</div>
+	);
+};

d3dee1734ecce0a3313443fcceb8764359025b96
Chris Kuhtz <christopherkuhtz@gmail.com>
2023-10-15 17:34:34 +0200

    assign actions to ai combatants

--- src/screens/BattleScreen/hooks/useBattleScreen.ts
+++ src/screens/BattleScreen/hooks/useBattleScreen.ts
@@ -1,17 +1,43 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Action } from '../../../interfaces/Action';
 import { Combatant } from '../../../interfaces/Combatant';
+import { actionGenerator } from '../../../testing/generators/actionGenerator';
+import { BattleScreenProps } from '../BattleScreen';
 import { updateCombatantInArray } from '../functions/updateCombatantInArray';
 
 export interface UseBattleScreen {
 	currentCombatants: Combatant[];
 	selectNextActionForCombatant: (id: string, action: Action) => void;
 }
-export const useBattleScreen = (
-	initialCombatants: Combatant[]
-): UseBattleScreen => {
+
+export const useBattleScreen = ({
+	initialCombatants,
+	opponentIds,
+	playerId,
+	allyId,
+}: BattleScreenProps): UseBattleScreen => {
 	const [currentCombatants, setCurrentCombatants] =
 		useState<Combatant[]>(initialCombatants);
+
+	const allPlayerCombatantsHaveMoves = useMemo(() => {
+		return currentCombatants
+			.filter((c) => c.pokemon.ownerId === playerId)
+			.every((c) => c.nextAction);
+	}, [currentCombatants, playerId]);
+
+	const allCombatantsHaveMoves = useMemo(() => {
+		return currentCombatants.every((c) => c.nextAction);
+	}, [currentCombatants]);
+
+	useEffect(() => {
+		if (allPlayerCombatantsHaveMoves && !allCombatantsHaveMoves) {
+			setCurrentCombatants(
+				currentCombatants.map((c) => {
+					return { ...c, nextAction: actionGenerator() };
+				})
+			);
+		}
+	}, [allCombatantsHaveMoves, allPlayerCombatantsHaveMoves, currentCombatants]);
 
 	const selectNextActionForCombatant = useCallback(
 		(id: string, action: Action) => {
@@ -36,5 +62,6 @@ export const useBattleScreen = (
 		},
 		[currentCombatants]
 	);
+
 	return { currentCombatants, selectNextActionForCombatant };
 };

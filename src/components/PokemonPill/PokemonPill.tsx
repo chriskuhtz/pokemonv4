600154b006a6cfb9cd66e6f447f2e4994497234b
Chris Kuhtz <christopherkuhtz@gmail.com>
2023-10-15 15:18:17 +0200

    add basic pokemonPill

--- src/components/PokemonPill/PokemonPill.tsx
+++ src/components/PokemonPill/PokemonPill.tsx
@@ -1,5 +1,5 @@
 import { Pokemon } from '../../interfaces/Pokemon';
-
+import './pokemonPill.css';
 export const PokemonPill = ({
 	pokemon,
 	onClick,
@@ -7,5 +7,12 @@ export const PokemonPill = ({
 	pokemon: Pokemon;
 	onClick: () => void;
 }): JSX.Element => {
-	return <div onClick={onClick}>{pokemon.name}</div>;
+	return (
+		<div className="pokemonPill" onClick={onClick}>
+			<img
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.dexId}.png`}
+			/>
+			<div>{pokemon.name}</div>
+		</div>
+	);
 };

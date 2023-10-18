a774812961771c2c536357f596e36d94689dd2ca
Chris Kuhtz <christopherkuhtz@gmail.com>
2023-10-18 21:21:34 +0200

    apply defeat

--- src/screens/BattleScreen/functions/assembleTurn.ts
+++ src/screens/BattleScreen/functions/assembleTurn.ts
@@ -1,27 +1,57 @@
 import { Combatant } from '../../../interfaces/Combatant';
 import { BattleSnapshot } from '../interfaces/BattleSnapshot';
+import { applyEffectsToTarget } from './applyEffectsToTarget';
 import { updateCombatantInArray } from './updateCombatantInArray';
 
 export const assembleTurn = (
 	combatants: Combatant[],
 	c: Combatant
 ): { snapshots: BattleSnapshot[]; updatedCombatants: Combatant[] } => {
-	const tempCombatants = updateCombatantInArray(combatants, {
+	const resSnapshots: BattleSnapshot[] = [];
+	let tempCombatants = updateCombatantInArray(combatants, {
 		...c,
 		nextAction: undefined,
 	});
 	const target = tempCombatants.find(
 		(potentialTarget) => potentialTarget.id === c.nextAction?.target
 	);
+
+	if ((c.state === 'ONFIELD' && !c.nextAction) || !target) {
+		return {
+			snapshots: [
+				{
+					messages: ['What is happening, no action or target'],
+					combatants: [...tempCombatants],
+				},
+			],
+			updatedCombatants: [...tempCombatants],
+		};
+	}
+
+	if (c.state === 'ONFIELD' && c.nextAction && target) {
+		resSnapshots.push({
+			messages: [
+				`${c.pokemon.name} used an attack against ${target?.pokemon.name}`,
+			],
+			combatants: [...tempCombatants],
+		});
+		const { updatedTarget, targetEffectMessages } = applyEffectsToTarget(
+			c.nextAction,
+			target
+		);
+		tempCombatants = updateCombatantInArray(tempCombatants, updatedTarget);
+		resSnapshots.push({
+			messages: [`That hit the spot`, ...targetEffectMessages],
+			combatants: [...tempCombatants],
+		});
+		return {
+			snapshots: resSnapshots,
+			updatedCombatants: [...tempCombatants],
+		};
+	}
+
 	return {
-		snapshots: [
-			{
-				messages: [
-					`${c.pokemon.name} used an attack against ${target?.pokemon.name}`,
-				],
-				combatants: [...tempCombatants],
-			},
-		],
+		snapshots: [],
 		updatedCombatants: [...tempCombatants],
 	};
 };

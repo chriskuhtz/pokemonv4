5ee0d7866bddeb15260602757a564af5f637f024
Chris Kuhtz <christopherkuhtz@gmail.com>
2023-11-29 22:35:27 +0100

    load savefiles

--- src/screens/SaveFileSelectionScreen/SaveFileSelection.tsx
+++ src/screens/SaveFileSelectionScreen/SaveFileSelection.tsx
@@ -1,10 +1,23 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useGetAllSaveFilesQuery } from '../../api/saveFileApi';
+import { setUserName } from '../../functions/setUserName';
+import { RoutesEnum } from '../../router/router';
 import { Pill } from '../../ui_components/Pill/Pill';
 import { ErrorScreen } from '../ErrorScreen/ErrorScreen';
 import { FetchingScreen } from '../FetchingScreen/FetchingScreen';
 
 export const SaveFileSelection = (): JSX.Element => {
 	const { data, isFetching, isError, isSuccess } = useGetAllSaveFilesQuery();
+	const navigate = useNavigate();
+
+	const selectSaveFile = useCallback(
+		(x: string) => {
+			setUserName(x);
+			navigate(RoutesEnum.overworld);
+		},
+		[navigate]
+	);
 
 	if (isFetching) {
 		return <FetchingScreen />;
@@ -13,7 +26,11 @@ export const SaveFileSelection = (): JSX.Element => {
 		return (
 			<div>
 				{Object.values(data).map((saveFile) => (
-					<Pill center={saveFile.username} />
+					<Pill
+						key={saveFile.username}
+						center={saveFile.username}
+						onClick={() => selectSaveFile(saveFile.username)}
+					/>
 				))}
 			</div>
 		);

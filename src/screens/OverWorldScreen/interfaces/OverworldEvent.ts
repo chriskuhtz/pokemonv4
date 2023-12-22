89353c9bad3335727f0bed53ff9064a15956c809
Chris Kuhtz <christopherkuhtz@gmail.com>
2023-12-22 17:47:01 +0100

    create portalEvent

--- src/screens/OverWorldScreen/interfaces/OverworldEvent.ts
+++ src/screens/OverWorldScreen/interfaces/OverworldEvent.ts
@@ -1,5 +1,19 @@
-export interface EncounterEvent {
+import { Direction } from '../../../interfaces/Direction';
+
+export interface BaseEvent {
+	type: 'ENCOUNTER' | 'PORTAL';
+}
+
+export interface EncounterEvent extends BaseEvent {
 	type: 'ENCOUNTER';
 }
 
-export type OverworldEvent = EncounterEvent;
+export interface PortalEvent extends BaseEvent {
+	type: 'PORTAL';
+	mapId: string;
+	x: number;
+	y: number;
+	orientation: Direction;
+}
+
+export type OverworldEvent = EncounterEvent | PortalEvent;

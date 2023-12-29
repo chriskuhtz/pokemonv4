1a25658134a5acbdc97696b5241a97425a88bc0d
Chris Kuhtz <christopherkuhtz@gmail.com>
2023-12-29 07:18:09 +0100

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
@@ -7,9 +6,7 @@ import './index.css';
 import { router } from './router/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
-	</React.StrictMode>
+	<Provider store={store}>
+		<RouterProvider router={router} />
+	</Provider>
 );

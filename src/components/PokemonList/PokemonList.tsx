c5a75bba59f23e2f91d0b53045742737dda388ad
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-03 22:20:56 +0100

    grid teamview

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,13 +1,13 @@
 import { OwnedPokemon } from '../../interfaces/SaveFile';
 import { PokemonListItem } from '../PokemonListItem/PokemonListItem';
-
+import './PokemonList.css';
 export interface PokemonListProps {
 	pokemon: OwnedPokemon[];
 }
 
 export const PokemonList = ({ pokemon }: PokemonListProps): JSX.Element => {
 	return (
-		<div>
+		<div className="pokemonList">
 			{pokemon.map((p) => (
 				<PokemonListItem pokemon={p} />
 			))}

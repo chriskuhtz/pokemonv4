b37014fbf44e0f71aadf9b123057ec5f5d4aca56
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-03 20:39:12 +0100

    implement pokeApi

--- src/api/store.ts
+++ src/api/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { mapApi } from './mapApi';
+import { pokeApi } from './pokeApi';
 import { saveFileApi } from './saveFileApi';
 
 export const store = configureStore({
@@ -9,13 +10,15 @@ export const store = configureStore({
 		// Add the generated reducer as a specific top-level slice
 		[saveFileApi.reducerPath]: saveFileApi.reducer,
 		[mapApi.reducerPath]: mapApi.reducer,
+		[pokeApi.reducerPath]: pokeApi.reducer,
 	},
 	// Adding the api middleware enables caching, invalidation, polling,
 	// and other useful features of `rtk-query`.
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware()
 			.concat(saveFileApi.middleware)
-			.concat(mapApi.middleware),
+			.concat(mapApi.middleware)
+			.concat(pokeApi.middleware),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors

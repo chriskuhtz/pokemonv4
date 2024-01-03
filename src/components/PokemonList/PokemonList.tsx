1085f073d4f940ec385846372a2af53a4200f56f
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-03 19:16:53 +0100

    display ids of List Members

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,3 +1,15 @@
-export const PokemonList = ({}: {}): JSX.Element => {
-	return <div></div>;
+import { OwnedPokemon } from '../../interfaces/SaveFile';
+
+export interface PokemonListProps {
+	pokemon: OwnedPokemon[];
+}
+
+export const PokemonList = ({ pokemon }: PokemonListProps): JSX.Element => {
+	return (
+		<div>
+			{pokemon.map((p) => (
+				<h1>{p.id}</h1>
+			))}
+		</div>
+	);
 };

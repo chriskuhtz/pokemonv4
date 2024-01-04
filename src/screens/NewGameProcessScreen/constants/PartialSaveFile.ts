266bcc4dcd249f3023b8ed9bc572ba0199a9be14
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-04 15:02:52 +0100

    starterTown map

--- src/screens/NewGameProcessScreen/constants/PartialSaveFile.ts
+++ src/screens/NewGameProcessScreen/constants/PartialSaveFile.ts
@@ -5,7 +5,7 @@ export const PARTIAL_SAVE_FILE: Partial<SaveFile> = {
 	position: { x: 0, y: 0 },
 	orientation: 'Down',
 	id: v4(),
-	currentMapId: 'healerTest',
+	currentMapId: 'starterTown',
 	mapProgress: {},
 	money: 5000,
 	inventory: {} as SaveFile['inventory'],

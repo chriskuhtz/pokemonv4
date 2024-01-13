c5ff7fc99bd56d9d9f2bf28eea93722495d8fb14
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-13 12:32:57 +0100

    improve FetchingScreen

--- src/screens/FetchingScreen/FetchingScreen.tsx
+++ src/screens/FetchingScreen/FetchingScreen.tsx
@@ -1,3 +1,51 @@
+import { CharacterSprite } from '../../components/CharacterSprite/CharacterSprite';
+import { useRotate } from '../../hooks/useRotate';
+
 export const FetchingScreen = (): JSX.Element => {
-	return <div>Something is loading</div>;
+	const currentOrientation = useRotate();
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flexDirection: 'column',
+				height: '100dvh',
+			}}
+		>
+			<h2>Something is loading</h2>
+			<div style={{ display: 'flex' }}>
+				<CharacterSprite
+					orientation={currentOrientation}
+					index={'136'}
+					style={
+						{
+							'--size': '80px',
+							marginRight: '-40px',
+						} as React.CSSProperties
+					}
+				/>{' '}
+				<CharacterSprite
+					orientation={currentOrientation}
+					index={'137'}
+					style={
+						{
+							'--size': '80px',
+							marginRight: '-40px',
+						} as React.CSSProperties
+					}
+				/>{' '}
+				<CharacterSprite
+					orientation={currentOrientation}
+					index={'138'}
+					style={
+						{
+							'--size': '80px',
+							marginRight: '-40px',
+						} as React.CSSProperties
+					}
+				/>
+			</div>
+		</div>
+	);
 };

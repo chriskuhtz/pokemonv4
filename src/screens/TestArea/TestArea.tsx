a273a2a309f5cd3ce8ad774098a88c74d3eff990
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-13 23:48:34 +0100

    implement cardWithImage

--- src/screens/TestArea/TestArea.tsx
+++ src/screens/TestArea/TestArea.tsx
@@ -1,5 +1,16 @@
-import { FetchingScreen } from '../FetchingScreen/FetchingScreen';
+import { CardWithImage } from '../../ui_components/CardWithImage/CardWithImage';
 
 export const TestArea = (): JSX.Element => {
-	return <FetchingScreen />;
+	return (
+		<div>
+			<CardWithImage
+				url={
+					'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
+				}
+				title="Ditto"
+				subtitle="The transforming Pokemon"
+				onClick={() => console.log('vrete')}
+			/>
+		</div>
+	);
 };

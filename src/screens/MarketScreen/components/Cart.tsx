21e8a38ecbd04ced656f03b65ef574366179fcf6
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-13 12:13:59 +0100

    extract components

---
+++ src/screens/MarketScreen/components/Cart.tsx
@@ -0,0 +1,48 @@
+import { ItemStack } from '../../../interfaces/Item';
+import { Pill } from '../../../ui_components/Pill/Pill';
+
+export const Cart = ({
+	cart,
+	removeFromCart,
+	totalCost,
+	money,
+	purchase,
+}: {
+	cart: ItemStack[];
+	removeFromCart: (x: ItemStack) => void;
+	totalCost: number;
+	money: number;
+	purchase: () => void;
+}): React.JSX.Element => {
+	if (cart.length === 0) {
+		return <></>;
+	}
+	return (
+		<div className="cart">
+			<div>
+				{cart.map((cartItem) => (
+					<h3 className="cartItem">
+						<span>
+							{cartItem.amount} {cartItem.item.id}
+						</span>
+						<span onClick={() => removeFromCart(cartItem)}>
+							{cartItem.amount === 1 ? 'X' : '-'}
+						</span>
+					</h3>
+				))}
+			</div>
+			<div>
+				<h3 className="cartTotal">
+					<span>TOTAL :</span>
+					<span>{totalCost}$</span>{' '}
+				</h3>
+				<Pill
+					disabled={totalCost > money}
+					className="buyButton"
+					center={'Buy'}
+					onClick={purchase}
+				/>
+			</div>
+		</div>
+	);
+};
---
+++ src/screens/MarketScreen/components/HydratedInventory.tsx
@@ -0,0 +1,24 @@
+import { MarketListItem } from '../../../components/MarketListItem/MarketListItem';
+import { ItemData } from '../../../shared/interfaces/ItemData';
+
+export const HydratedInventory = ({
+	hydratedInventory,
+	addToCart,
+}: {
+	hydratedInventory?: ItemData[];
+	addToCart: (x: ItemData) => void;
+}): React.JSX.Element => {
+	if (!hydratedInventory) {
+		return <></>;
+	}
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', gap: '.5rem' }}>
+			{hydratedInventory.map((inventoryItem) => (
+				<MarketListItem
+					item={inventoryItem}
+					onClick={() => addToCart(inventoryItem)}
+				/>
+			))}
+		</div>
+	);
+};

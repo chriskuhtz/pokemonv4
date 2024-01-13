9de09b8fc0862ba0afb252aa19c7bb6627a15f3f
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-13 12:33:34 +0100

    extract useRotate

--- src/screens/NewGameProcessScreen/NewGameProcess.tsx
+++ src/screens/NewGameProcessScreen/NewGameProcess.tsx
@@ -7,6 +7,7 @@ import {
 import { CharacterSprite } from '../../components/CharacterSprite/CharacterSprite';
 import { isValidSaveFile } from '../../functions/isValidSaveFile';
 import { setUserName } from '../../functions/setUserName';
+import { useRotate } from '../../hooks/useRotate';
 import { SaveFile } from '../../interfaces/SaveFile';
 import { RoutesEnum } from '../../router/router';
 import { Pill } from '../../ui_components/Pill/Pill';
@@ -14,7 +15,6 @@ import { ErrorScreen } from '../ErrorScreen/ErrorScreen';
 import { FetchingScreen } from '../FetchingScreen/FetchingScreen';
 import { SpriteSelection } from './components/SpriteSelection';
 import { PARTIAL_SAVE_FILE } from './constants/PartialSaveFile';
-import { useRotate } from './hooks/useRotate';
 
 export const NewGameProcess = (): JSX.Element => {
 	const currentOrientation = useRotate();

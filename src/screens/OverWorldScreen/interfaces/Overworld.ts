76decddecf7af2e3ec48a08b100c233c00bbc202
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-01-13 18:55:48 +0100

    add grass maps

--- src/screens/OverWorldScreen/interfaces/Overworld.ts
+++ src/screens/OverWorldScreen/interfaces/Overworld.ts
@@ -4,7 +4,7 @@ import { OverworldEvent } from './OverworldEvent';
 export type BaseTileId = 'beach' | 'caveFloor' | 'cobblestone' | 'grass';
 
 export const BaseTileMap: Record<BaseTileId, Record<number, number>> = {
-	grass: { 1: 100 },
+	grass: { 1: 10, 2: 20, 3: 30, 4: 40, 5: 100 },
 	caveFloor: { 1: 100 },
 	beach: { 1: 30, 2: 60, 3: 90, 4: 100 },
 	cobblestone: { 1: 18, 2: 36, 3: 54, 4: 72, 5: 90, 6: 100 },
